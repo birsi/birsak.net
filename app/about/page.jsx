018c82ecefda61261f04edd63f4103d4e825bcb5
Michael Birsak <michael@birsak.net>
2023-06-11 22:14:44 +0200

    Add content

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,100 @@
+import { OpenInNewTabIcon } from "../components/icons";
+
 export default function About() {
+  const skills = [
+    "JavaScript",
+    "TypeScript",
+    "Java",
+    "AWS",
+    "Node.js",
+    "React.js",
+    "Meteor.js",
+    "Next.js",
+    "Express",
+    "Koa",
+    "GSAP",
+    "Framer/Motion",
+    "Angular",
+    "MongoDB",
+    "MySQL",
+    "Postgres",
+    "Redis",
+    "Elasticsearch",
+    "Docker",
+    "Kubernetes",
+    "OpenShift",
+    "ArgoCD",
+    "Git",
+    "Github",
+    "Gitlab",
+    "Bitbucket",
+    "SVN",
+    "Equinox (OSGi)",
+    "Eclipse RCP/RAP",
+    "Maven",
+    "Gradle",
+    "PHP",
+    "CakePHP",
+  ];
+
   return (
-    <main className="flex min-h-screen justify-center items-center selection:text-black">
-      <div>Hi, I'm Michael and I am a Software Engineer at heart.</div>
+    <main className="pt-8 max-w-3xl">
+      <h2>About me</h2>
+      <p className="pb-5">A little story about me!</p>
+      <h2>Experience</h2>
+      <ul className="pb-5">
+        <li className="mb-3">
+          <div>Director of Engineering</div>
+          <a href="" className="text-xs hover:text-[#27d17f] transition-all">
+            Bitpanda
+          </a>
+        </li>
+        <li className="mb-3">
+          <div>Chief Technology Officer</div>
+          <a href="" className="text-xs hover:text-[#fe816a] transition-all">
+            hi.health
+          </a>
+        </li>
+        <li className="mb-3">
+          <div>Engineering Manager</div>
+          <a href="" className="text-xs hover:text-[#ffffff] transition-all">
+            adidas runtastic
+          </a>
+        </li>
+        <li className="mb-3">
+          <div>Lead Software Engineer, Consultant & TAM</div>
+          <a href="" className="text-xs hover:text-[#113388] transition-all">
+            Allianz Technology
+          </a>
+        </li>
+        <li className="mb-3">
+          <div>Co-Founder, Software Engineer</div>
+          <a href="" className="text-xs hover:text-[#ffffff] transition-all">
+            WILD
+          </a>
+        </li>
+        <li>
+          <div>Co-Founder, Software Engineer</div>
+          <a href="" className="text-xs hover:text-[#ffffff] transition-all">
+            creativecocks
+          </a>
+        </li>
+      </ul>
+      <h2>Skills & Expertise</h2>
+      <ul>
+        {skills.map((value, index, arr) => {
+          return (
+            <li
+              key={index}
+              className="inline-block list-none px-2.5 py-1 rounded-lg mr-2 mb-1 bg-zinc-700 text-xs font-medium"
+            >
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+      <h2>Tech I've been working recently with</h2>
+      <h2>Tech I've worked with in the past</h2>
     </main>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,30 @@
+import { OpenInNewTabIcon } from "./components/icons";
+
 export default function Home() {
   return (
-    <main className="block pt-12 md:flex">
-      <div className="flex-none text-zinc-300 md:flex-1 md:pl-44">
-        Hello, my name is Michael and I am a Software Engineer at heart.
-      </div>
+    <main className="pt-8 max-w-3xl">
+      <h2>Hello!</h2>
+      <p className="pb-5">
+        I&apos;m Michael and I&apos;m a Software Engineer at heart. I&apos;m{" "}
+        <span className="font-semibold">Director of Engineering</span> at{" "}
+        <a
+          href="https://www.bitpanda.com/en"
+          target="_blank"
+          className="text-[#27d17f] fill-[#27d17f] font-semibold hover:text-white hover:fill-white transition-all"
+        >
+          Bitpanda <OpenInNewTabIcon />
+        </a>
+        , where my teams build business critical applications and services for
+        our Operations & Financial Crime Prevention departments. I love to build
+        teams, get high-quality products to market and build companies up for
+        success! Though people say I seem happiest when I can get my hands
+        dirty, build and create things and simply get things done.
+      </p>
+      <h2>Latest Blog Posts</h2>
+      <ul className="list-disc list-inside">
+        <li>Service ownership in different company sizes</li>
+        <li>How I passed my AWS certifications</li>
+      </ul>
     </main>
   );
 }
